9afc28ad577eb7b590a6ffab12da28c49c66e2c3
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2022-11-29 20:58:36 +0000

    aaaa

--- run.py
+++ run.py
@@ -72,8 +72,7 @@ while True:
     thresh = cv2.threshold(frameDelta, 5, 255,
                            cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
-    (_, cnts, _) = cv2.findContours(thresh.copy(),
-                                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (_, cnts) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # loop over the contours
     for c in cnts:
