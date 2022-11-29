a943498b955d980daa2ded25faa48e6d2bd868c8
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2022-11-29 21:06:12 +0000

    capture 720w

--- run.py
+++ run.py
@@ -50,7 +50,7 @@ while True:
         break
 
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
+    frame = imutils.resize(frame, width=720)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
